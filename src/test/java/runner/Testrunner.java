8db65de9d657427fd8b260b0ee5cda9ca50cfa78
kowsika34 <kowsika.shrimv@gmail.com>
2026-06-19 00:04:10 -0400

    testrunner

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -1,18 +1,14 @@
 package runner;
 
-import org.junit.runner.RunWith;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
-
-@CucumberOptions(features="src/test/resources/features", glue = {"StepDefinitions"}, plugin = {"pretty", "html:target/report.html"})
-
-public class Testrunner extends AbstractTestNGCucumberTests{
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "StepDefinitions",
+    plugin = {"pretty", "html:target/cucumber-report.html"},
+    monochrome = true
+)
+public class Testrunner extends AbstractTestNGCucumberTests {
 	
-	
-	
-
 }
