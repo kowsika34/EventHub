81a84e58fa96c1b1ff5ca22506372bcd64aaad53
kowsika34 <kowsika.shrimv@gmail.com>
2026-06-18 23:45:07 -0400

    updare runner

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -4,12 +4,14 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features="src/test/resources/features", glue = {"StepDefinitions"}, plugin = {"pretty", "html:target/report.html"})
 
-public class Testrunner {
+public class Testrunner extends AbstractTestNGCucumberTests{
+	
 	
 	
 
